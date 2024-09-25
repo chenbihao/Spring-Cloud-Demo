0a4c7e6adbac057970d5d64698ec9c6a5c4875aa
被水淹没 <994523036@qq.com>
2024-09-25 21:26:11 +0800

    Stream 错误处理（重试、降级、死信）

--- ch7-Spring Cloud 高级（Stream 消息驱动）/coupon-customer-serv/coupon-customer-impl/src/main/java/cn/noobbb/coupon/customer/event/CouponConsumer.java
+++ ch7-Spring Cloud 高级（Stream 消息驱动）/coupon-customer-serv/coupon-customer-impl/src/main/java/cn/noobbb/coupon/customer/event/CouponConsumer.java
@@ -5,6 +5,8 @@ import cn.noobbb.coupon.customer.service.intf.CouponCustomerService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.support.ErrorMessage;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -36,5 +38,27 @@ public class CouponConsumer {
             customerService.deleteCoupon(params.get(0), params.get(1));
         };
     }
+
+    //@ServiceActivator(inputChannel = "errorChannel")
+//    @ServiceActivator(inputChannel = "request-coupon-topic.add-coupon-group.errors")
+//    public void requestCouponFallback(ErrorMessage errorMessage) throws Exception {
+//        log.info("consumer error fallback: {}", errorMessage);
+//        // 实现自己的逻辑，例如告警通知
+//        // 告警完后可以考虑是丢弃信息，还是发送到某个队列里等错误处理完后再把消息重新投递回原来的队列里
+//
+//    }
+
+    @Bean
+    public Consumer<ErrorMessage> requestCouponErrorHandler() {
+        return v -> {
+            log.info("consumer error handler: {}", v);
+            // 实现自己的逻辑，例如告警通知
+            // 告警完后可以考虑是丢弃信息，还是发送到某个队列里等错误处理完后再把消息重新投递回原来的队列里
+
+//            // 这里抛出异常转入死信队列
+//            throw new IllegalArgumentException("抛出异常转入死信队列");
+        };
+    }
+
 }
 
